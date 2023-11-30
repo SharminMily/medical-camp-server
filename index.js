f7922bc40d8942810c2d389d02e2704217687a02
SharminMily <sharminmily039@gmail.com>
2023-11-30 19:16:20 +0600

    health care data loade

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/healthCare', async(req, res) => {
+        const result = await healthCareCollection.find().toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
